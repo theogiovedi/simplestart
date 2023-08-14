267a08d9dae5de21bb61d4c275f339e69ef6b485
Théo Giovedi <tmgiovedi@gmail.com>
2023-08-14 13:33:25 -0300

    feat: add code for bookmarks

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -5,13 +5,16 @@ import { useEffect, useState } from 'react'
 export default function Settings() {
   const [background, setBackground] = useState('');
   const [color, setColor] = useState('');
+  const [bookmarks, setBookmarks] = useState('');
 
   useEffect(() => {
     window.localStorage.setItem('background', '#2B3E1D');
-    window.localStorage.setItem('image', 'https://wallpapercave.com/uwp/uwp2147701.jpeg');
     window.localStorage.setItem('color', '#e6e6e6');
+    let foo = [{ name: "The Verge", url: "https://theverge.com" }, { name: "Email", url: "https://gmail.com" }, { name: "Awwwards", url: "https://awwwards.com" }, { name: "UOL", url: "https://uol.com.br" }, { name: "Qual a Cor?", url: "https://theogiovedi.github.io/qual-a-cor" }];
+    window.localStorage.setItem('bookmarks', JSON.stringify(foo));
     setBackground(window.localStorage.getItem('background'));
     setColor(window.localStorage.getItem('color'));
+    setBookmarks(window.localStorage.getItem('bookmarks'));
   });
 
   return (
@@ -22,18 +25,20 @@ export default function Settings() {
         <label className={styles.label}>
           Background Image
           <div className={styles.feature}>
-            <input type="text" className={styles.input}/>
-            <button className={styles.button}><i className="bi bi-plus-lg"></i></button>
+            <input type="file" className={styles.input}/>
           </div>
         </label>
         <label className={styles.label}>
           Bookmarks
-          <div className={styles.feature}>
+          <form className={styles.feature}>
+            <input type="text" className={styles.input}/>
             <input type="text" className={styles.input}/>
             <button className={styles.button}><i className="bi bi-plus-lg"></i></button>
-          </div>
+          </form>
           <ul>
-            {localStorage.getItem}
+            {
+              JSON.parse(bookmarks).map(item => <li><a href={item.url}>{item.name}</a></li>)
+            }
           </ul>
         </label>
       </main>
